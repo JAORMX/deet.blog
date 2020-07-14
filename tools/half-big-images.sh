526a7d58901a03bc68a5c5aaa4e5b80fcbd9d827
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2020-07-14 11:22:52 +0300

    Fix half-big-images.sh script

--- tools/half-big-images.sh
+++ tools/half-big-images.sh
@@ -4,4 +4,4 @@
 
 top_n=${1:-5}
 
-./list-big-images.sh "$top_n" | awk '{print $2}' | xargs -n1 mogrify -resize 50%
+./tools/list-big-images.sh "$top_n" | awk '{print $2}' | xargs -n1 mogrify -resize 50%
