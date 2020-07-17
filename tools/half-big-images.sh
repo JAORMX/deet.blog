e1af5a697b130d62a6f6780bebb45418372dea02
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2020-07-17 09:54:03 +0300

    Modify tool names

--- tools/half-big-images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Requires the ImageMagick package
-
-top_n=${1:-5}
-
-./tools/list-big-images.sh "$top_n" | awk '{print $2}' | xargs -n1 mogrify -resize 50%
---
+++ tools/half-top-big-images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Requires the ImageMagick package
+
+top_n=${1:-5}
+
+./tools/list-top-big-images.sh "$top_n" | awk '{print $2}' | xargs -n1 mogrify -resize 50%
